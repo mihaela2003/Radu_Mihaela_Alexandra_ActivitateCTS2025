99da41042c1221adc244bdc29de1b40a6e4d6ff3
mihaela2003 <mihaelaradu975@gmail.com>
2025-04-09 00:22:26 +0300

    test 1 prototype

---
+++ Prototype/Restaurant_B4/src/restaurantPrototype/clase/Client.java
@@ -0,0 +1,41 @@
+package restaurantPrototype.clase;
+
+public class Client extends ClientPrototype{
+    private String nume;
+    private String nrTelefon;
+
+    public Client(String tipRezervare, int nrPersoane, String dataRezervare, int oraRezervare, String nume, String nrTelefon) {
+        super(tipRezervare, nrPersoane, dataRezervare, oraRezervare);
+        this.nume = nume;
+        this.nrTelefon = nrTelefon;
+    }
+
+    public Client() {
+        super();
+    }
+
+    @Override
+    public ClientPrototype copiaza(String dataRezervare) {
+        Client client = new Client();
+        client.tipRezervare = this.tipRezervare;
+        client.nrPersoane = this.nrPersoane;
+        client.dataRezervare = this.dataRezervare;
+        client.oraRezervare = this.oraRezervare;
+        client.nume = this.nume;
+        client.nrTelefon = this.nrTelefon;
+        return client;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Client{");
+        sb.append("nume='").append(nume).append('\'');
+        sb.append(", nrTelefon='").append(nrTelefon).append('\'');
+        sb.append(", tipRezervare='").append(tipRezervare).append('\'');
+        sb.append(", nrPersoane=").append(nrPersoane);
+        sb.append(", dataRezervare='").append(dataRezervare).append('\'');
+        sb.append(", oraRezervare=").append(oraRezervare);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ Prototype/Restaurant_B4/src/restaurantPrototype/clase/ClientPrototype.java
@@ -0,0 +1,51 @@
+package restaurantPrototype.clase;
+
+public abstract class ClientPrototype {
+    protected String tipRezervare;
+    protected int nrPersoane;
+    protected String dataRezervare;
+    protected int oraRezervare;
+
+    public ClientPrototype(String tipRezervare, int nrPersoane, String dataRezervare, int oraRezervare) {
+        if (tipRezervare.length() > 0) {
+            this.tipRezervare = tipRezervare;
+        } else {
+            this.tipRezervare = "nu conteaza";
+        }
+        if (nrPersoane > 2) {
+            this.nrPersoane = nrPersoane;
+        } else {
+            this.nrPersoane = 2;
+        }
+        if(dataRezervare.length() > 0) {
+            this.dataRezervare = dataRezervare;
+        } else {
+            this.dataRezervare = "25 ianuarie 2024";
+        }
+        if(oraRezervare >= 10 && oraRezervare <= 22) {
+            this.oraRezervare = oraRezervare;
+        } else {
+            this.oraRezervare = 10;
+        }
+    }
+
+    public ClientPrototype() {
+        this.tipRezervare = "nu conteaza";
+        this.nrPersoane = 2;
+        this.dataRezervare = "25 ianuarie 2024";
+        this.oraRezervare = 10;
+    }
+
+    public abstract ClientPrototype copiaza(String dataRezervare);
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ClientPrototype{");
+        sb.append("tipRezervare='").append(tipRezervare).append('\'');
+        sb.append(", nrPersoane=").append(nrPersoane);
+        sb.append(", dataRezervare='").append(dataRezervare).append('\'');
+        sb.append(", oraRezervare=").append(oraRezervare);
+        sb.append('}');
+        return sb.toString();
+    }
+}
